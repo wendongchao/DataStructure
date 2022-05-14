b1bbdfb90fe2b6595cd77a15493ac2435c5bd2f4
wendongchao <dongchao1860369@sina.com>
2022-05-14 15:01:16 +0800

    添加test依赖

--- src/main/java/com/wdc/structure/sparsearray/SparseArray.java
+++ src/main/java/com/wdc/structure/sparsearray/SparseArray.java
@@ -8,7 +8,7 @@ package com.wdc.structure.sparsearray;
 public class SparseArray {
     public static void main(String[] args) {
         // 创建一个原始的二维数组 11 * 11
-        // 0: 表示没有棋子， 1 表示 黑子 2 表蓝子
+        // 0: 表示没有棋子， 1: 黑子 2: 蓝子
         int chessArr1[][] = new int[11][11];
         chessArr1[1][2] = 1;
         chessArr1[2][3] = 2;
